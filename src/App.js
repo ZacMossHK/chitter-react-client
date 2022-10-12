e158c1f263ae36d70375b4b6de5a3e782073bbd2
Zac <zac@zacmoss.co.uk>
2022-10-12 17:52:07 +0100

    made fetch request to test api/client relationship

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import logo from "./logo.svg";
 import "./App.css";
-import React from "react";
-
-// eslint-disable-next-line no-undef
-export default App = () => {
+import { useEffect, useState } from "react";
+const App = () => {
+  const [text, setText] = useState();
+  useEffect(() => {
+    fetch("http://localhost:9000/")
+      .then((response) => response.text())
+      .then(setText);
+  }, []);
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p>{text}</p>
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -23,3 +25,5 @@ export default App = () => {
     </div>
   );
 };
+
+export default App;
