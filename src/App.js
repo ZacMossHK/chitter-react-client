56bc0afea73a0322c326240baa015b1fa2d7fd9f
Zac <zac@zacmoss.co.uk>
2022-10-21 20:02:13 +0100

    App renders the Chitter Header

--- src/App.js
+++ src/App.js
@@ -1,27 +1,7 @@
-import logo from "./logo.svg";
-import "./App.css";
-import { useEffect, useState } from "react";
 const App = () => {
-  const [text, setText] = useState();
-  useEffect(() => {
-    fetch("http://localhost:9000/")
-      .then((response) => response.text())
-      .then(setText);
-  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>{text}</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Chitter</h1>
     </div>
   );
 };
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,11 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe("App", () => {
+  it("renders the Chitter Header", () => {
+    expect.assertions(1);
+    render(<App />);
+    const linkElement = screen.getByText(/Chitter/);
+    expect(linkElement).toBeInTheDocument();
+  });
 });
