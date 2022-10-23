a9c4da7f4a930b1118bb668ae89fbd0d56f799cb
Zac <zac@zacmoss.co.uk>
2022-10-23 12:06:36 +0100

    App renders a peep

--- src/App.js
+++ src/App.js
@@ -1,11 +1,55 @@
+import React, { useEffect, useState } from "react";
+
 const App = () => {
+  // const [passwordValue, setPasswordValue] = useState("");
+  const [peeps, setPeeps] = useState([]);
+  // const handleChange = (event) => {
+  //   if (event.target.id === "username-login-input") {
+  //     setPasswordValue(event.target.value);
+  //   }
+  // };
+  useEffect(() => {
+    fetch("")
+      .then((response) => response.json())
+      .then((result) => {
+        if (result.length) setPeeps(result);
+      });
+  }, []);
+
+  const renderPeeps = () => {
+    if (peeps.length) {
+      return (
+        <div>
+          <p>@{peeps[0].username}</p>
+          <p>{peeps[0].body}</p>
+          <p>Posted at {new Date(peeps[0].createdAt).toString()}</p>
+          <p>♡ {peeps[0].likes.length}</p>
+        </div>
+      );
+    } else {
+      return <p>No peeps</p>;
+    }
+  };
   return (
     <div>
       <h1>Chitter</h1>
-      <p>username</p>
-      <p>password</p>
-      <p>Log in</p>
-      <p>Sign up</p>
+      <form>
+        <input
+          id="username-login-input"
+          name="username"
+          defaultValue=""
+          placeholder="username"
+        />
+        <input
+          id="password-login-input"
+          name="password"
+          defaultValue=""
+          placeholder="password"
+        />
+        <input id="log in" type="submit" value="log in" />
+      </form>
+      <p>sign up</p>
+      {renderPeeps()}
     </div>
   );
 };
--- src/App.test.js
+++ src/App.test.js
@@ -2,16 +2,66 @@ import { render, screen } from "@testing-library/react";
 import App from "./App";
 
 describe("App", () => {
+  beforeEach(() => {
+    fetch.resetMocks();
+    fetch.mockResponse(JSON.stringify([]));
+  });
+
   it("renders the Chitter Header", () => {
     render(<App />);
-    expect(screen.getByText(/Chitter/)).toBeInTheDocument();
+    const text = screen.getByText(/Chitter/);
+    expect(text).toBeInTheDocument();
   });
 
   it("renders the login options", () => {
     render(<App />);
-    expect(screen.getByText(/username/)).toBeInTheDocument();
-    expect(screen.getByText(/password/)).toBeInTheDocument();
-    expect(screen.getByText(/Log in/)).toBeInTheDocument();
-    expect(screen.getByText(/Sign up/)).toBeInTheDocument();
+    expect(screen.getByPlaceholderText(/username/)).toBeInTheDocument();
+    expect(screen.getByPlaceholderText(/password/)).toBeInTheDocument();
+    expect(screen.getByText(/log in/)).toBeInTheDocument();
+    expect(screen.getByText(/sign up/)).toBeInTheDocument();
+  });
+
+  it("renders a peep", async () => {
+    const peep = [
+      {
+        username: "foo",
+        body: "first peep",
+        createdAt: new Date(2022, 10, 11),
+        likes: ["id1, id2"],
+      },
+    ];
+    fetch.mockResponseOnce(JSON.stringify(peep));
+    render(<App />);
+    const username = await screen.findByText(/@foo/);
+    const body = await screen.findByText(/first peep/);
+    const createdAt = await screen.findByText(
+      "Posted at Fri Nov 11 2022 00:00:00 GMT+0000 (Greenwich Mean Time)"
+    );
+    const likes = await screen.findByText(/♡ 1/);
+    [(username, body, createdAt, likes)].forEach((key) =>
+      expect(key).toBeInTheDocument()
+    );
+  });
+
+  it("renders a differnt peep", async () => {
+    const peep = [
+      {
+        username: "bar",
+        body: "second peep",
+        createdAt: new Date(2022, 10, 23),
+        likes: ["id1", "id2"],
+      },
+    ];
+    fetch.mockResponseOnce(JSON.stringify(peep));
+    render(<App />);
+    const username = await screen.findByText(/@bar/);
+    const body = await screen.findByText(/second peep/);
+    const createdAt = await screen.findByText(
+      "Posted at Wed Nov 23 2022 00:00:00 GMT+0000 (Greenwich Mean Time)"
+    );
+    const likes = await screen.findByText(/♡ 2/);
+    [(username, body, createdAt, likes)].forEach((key) =>
+      expect(key).toBeInTheDocument()
+    );
   });
 });
