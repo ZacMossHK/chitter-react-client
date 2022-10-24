d1e4c5bccbc0e4ca733b91501cafd014028024b8
Zac <zac@zacmoss.co.uk>
2022-10-24 12:16:40 +0100

    SignUpForm sets the user as the signed up user

--- src/components/signUpForm.js
+++ src/components/signUpForm.js
@@ -1,11 +1,17 @@
 const SignUpForm = ({ setUser }) => {
+  const handleSubmitButtonClick = async () => {
+    fetch("");
+    setUser({ _id: 1, username: "foo" });
+  };
   return (
     <div>
       <h2>Sign up!</h2>
       <input name="username" placeholder="username"></input>
       <input name="password" placeholder="password"></input>
       <input name="email" placeholder="email"></input>
-      <button name="submit">submit</button>
+      <button name="submit" onClick={handleSubmitButtonClick}>
+        submit
+      </button>
       <button name="back">back</button>
     </div>
   );
--- src/components/signUpForm.test.js
+++ src/components/signUpForm.test.js
@@ -1,7 +1,11 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen, waitFor } from "@testing-library/react";
 import SignUpForm from "./signUpForm";
+import userEvent from "@testing-library/user-event";
 
 describe("SignUpForm", () => {
+  beforeEach(() => {
+    fetch.resetMocks();
+  });
   it("renders the options", () => {
     render(<SignUpForm />);
     expect(screen.getByText(/Sign up!/)).toBeInTheDocument();
@@ -11,8 +15,19 @@ describe("SignUpForm", () => {
     expect(screen.getByRole("button", { name: "submit" })).toBeInTheDocument();
     expect(screen.getByRole("button", { name: "back" })).toBeInTheDocument();
   });
-  // it("sets the user as the signed up user", () => {
-  //   const mockSetUser = jest.fn()
-  //   render <si
-  // })
+  it("sets the user as the signed up user", async () => {
+    const mockSetUser = jest.fn();
+    const user = { _id: 1, username: "foo" };
+    fetch.mockResponseOnce(JSON.stringify(user));
+    render(<SignUpForm setUser={mockSetUser} />);
+    await userEvent.type(screen.getByPlaceholderText(/username/), "foo");
+    await userEvent.type(screen.getByPlaceholderText(/password/), "password");
+    await userEvent.type(
+      screen.getByPlaceholderText(/email/),
+      "email@email.com"
+    );
+    await userEvent.click(screen.getByRole("button", { name: "submit" }));
+    expect(fetch).toHaveBeenCalled();
+    expect(mockSetUser).toHaveBeenCalledWith(user);
+  });
 });
