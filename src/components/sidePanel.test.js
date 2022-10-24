9603cb28034719c7b56f799486a986e1ea9dfeb7
Zac <zac@zacmoss.co.uk>
2022-10-24 14:18:50 +0100

    LoginForm renders the default panel

--- src/components/sidePanel.test.js
+++ src/components/sidePanel.test.js
@@ -1,10 +1,20 @@
 import { render, screen } from "@testing-library/react";
 import SidePanel from "./sidePanel";
+import LoginForm from "./loginForm";
+import PeepForm from "./peepForm";
+jest.mock("./loginForm");
+jest.mock("./peepForm");
 
 describe("SidePanel", () => {
-  it("renders the Chitter Header", () => {
+  beforeEach(() => {
+    // LoginForm.resetMocks();
+    // PeepForm.resetMocks();
+  });
+  it("renders the default panel", () => {
+    LoginForm.mockImplementation(() => <p>Login</p>);
     render(<SidePanel />);
     const text = screen.getByText(/Chitter/);
     expect(text).toBeInTheDocument();
+    expect(screen.getByText(/Login/)).toBeInTheDocument();
   });
 });
