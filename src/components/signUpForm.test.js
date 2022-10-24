7dddfd320049439452582648390c71db633dfbbe
Zac <zac@zacmoss.co.uk>
2022-10-24 12:21:33 +0100

    removed WaitFor from SignUpForm.test

--- src/components/signUpForm.test.js
+++ src/components/signUpForm.test.js
@@ -1,4 +1,4 @@
-import { render, screen, waitFor } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import SignUpForm from "./signUpForm";
 import userEvent from "@testing-library/user-event";
 
