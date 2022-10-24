2b3b54eaa5146201ac55b32b67a9c5e85959f77b
Zac <zac@zacmoss.co.uk>
2022-10-24 14:36:45 +0100

    SidePanel renders SignUpForm if signUpFormVisible = true

--- src/components/sidePanel.js
+++ src/components/sidePanel.js
@@ -1,13 +1,21 @@
 import { useState } from "react";
 import LoginForm from "./loginForm";
 import PeepForm from "./peepForm";
+import SignUpForm from "./signUpForm";
 
 const SidePanel = () => {
   const [user, setUser] = useState(null);
+  const [signUpFormVisible, setSignUpFormVisible] = useState(false);
 
   const renderPanel = () => {
+    if (signUpFormVisible) return <SignUpForm />;
     if (user) return <PeepForm user={user} setUser={setUser} />;
-    return <LoginForm setUser={setUser} />;
+    return (
+      <LoginForm
+        setUser={setUser}
+        setSignUpFormVisible={setSignUpFormVisible}
+      />
+    );
   };
   return (
     <div>
--- src/components/sidePanel.test.js
+++ src/components/sidePanel.test.js
@@ -3,8 +3,10 @@ import SidePanel from "./sidePanel";
 import LoginForm from "./loginForm";
 import PeepForm from "./peepForm";
 import { useEffect } from "react";
+import SignUpForm from "./signUpForm";
 jest.mock("./loginForm");
 jest.mock("./peepForm");
+jest.mock("./signUpForm");
 
 describe("SidePanel", () => {
   it("renders the default panel", () => {
@@ -27,5 +29,18 @@ describe("SidePanel", () => {
     render(<SidePanel />);
     expect(screen.queryByText(/Login/)).toBeNull();
     expect(screen.getByText(/foo/)).toBeInTheDocument();
+  });
+
+  it("renders SignUpForm if signUpFormVisible = true", () => {
+    LoginForm.mockImplementation(({ setSignUpFormVisible }) => {
+      useEffect(() => {
+        setSignUpFormVisible(true);
+      }, [setSignUpFormVisible]);
+      return <p>Login</p>;
+    });
+    SignUpForm.mockImplementation(() => <p>submit</p>);
+    render(<SidePanel />);
+    expect(screen.queryByText(/Login/)).toBeNull();
+    expect(screen.getByText(/submit/)).toBeInTheDocument();
   });
 });
